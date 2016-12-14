4f9f83e90c638841a6ef16a0f86422fcd40f2557
Alexander Wilkinson <alexwilkinson@gmail.com>
2016-12-14 12:44:13 +0800

    Inital Commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fixed image</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+	integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" href="style/style.css"/>
+
+	<script src="javascript/app.js"></script>
+</head>
+<body>
+
+<nav>
+	<a id="home" href="#homeSection">HOME</a>
+	<a id="about" href="#aboutSection">ABOUT</a>
+	<a id="product" href="#productSection">PRODUCT</a>
+	<a id="location" href="#locationSection">LOCATION</a>
+
+
+
+</nav>
+
+<div id="homeSection" class="sect sect-landing">
+	<div id="homeContent" class="sect sect-landing-content">
+		<p>main content in the home sectionsss</p>
+	</div>
+</div>
+
+<div id="social-media" class="subSect">
+	<div class="social-media social-icons">
+		<i class="fa fa-twitter-square fa-5x" aria-hidden="true"></i>
+			<i class="fa fa-facebook-official fa-5x" aria-hidden="true"></i>
+			<i class="fa fa-instagram fa-5x" aria-hidden="true"></i>
+			<i class="fa fa-envelope-square fa-5x" aria-hidden="true"></i>
+	</div>
+	<div class="social-media social-content">
+		With promotions ran every week be sure to follow us on one, or more, of the many social platforms avaliable.
+	</div>
+</div>
+
+<div id="aboutSection" class="sect sect-about">
+	<div class="sect sect-about-content">
+		<p>the about section</p>
+	</div>
+</div>
+
+<div id="" class="subSect">
+	<div class=" game-developers" >
+		<img src="images/ffg.png" height="200px" width="200px">
+		<img src="images/asmodee.png" height="200px" width="200px">
+		<img src="images/wizards.png" height="200px" width="300px">
+
+	</div>
+</div>
+
+<div id="productSection" class="sect sect-product">
+	<div class="sect sect-product-content">
+		<p>The product section goes heress</p>
+	</div>
+</div>
+
+<div id="locationSection" class="subSect subSect-location">
+<div class="location-text">Come and find use anytime at our store located conveniantly just a few minutes from Bukit Panjang station,<br> easily accessaible with both the Downtown line MRT or the Bukit Panjang LRT.</div>
+
+<div class="google-maps">
+	<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d3988.663970590043!2d103.76130007543779!3d1.3782888691886774!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ssg!4v1481296569264" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+<div class="copyright">Copyright 2017</div>
+</div>
+
+</body>
+</html>
